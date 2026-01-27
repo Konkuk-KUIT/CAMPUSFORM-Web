02c07080ac06ea032c224b8a481975841a5b76e0
Ssopaa <tpgus1231234@naver.com>
2026-01-27 16:25:49 +0900

    [fix] 홈화면 파일 정리
    미사용 컴포넌트 삭제

--- src/components/home/ScheduleList.tsx
+++ src/components/home/ScheduleList.tsx
@@ -41,7 +41,7 @@ export default function ScheduleList({ selectedDate = new Date(), schedules = []
       ) : (
         <div className="flex flex-col gap-[12px]">
           {schedules.map((item, index) => (
-            <ScheduleSummaryCard 
+            <Schedule 
               key={index}
               title={item.title}
               timeRange={item.timeRange}
