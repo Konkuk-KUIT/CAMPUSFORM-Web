a659a728d202f8aef6b2380696d632c466f03e4a
Chosehyun <tpgus1231234@naver.com>
2026-02-13 23:10:44 +0900

    [style] 알림헤더 스타일 통일

--- src/components/home/notification/HeaderNotification.tsx
+++ src/components/home/notification/HeaderNotification.tsx
@@ -17,23 +17,21 @@ export default function HeaderNotification({
   rightElement,
 }: HeaderNotificationProps) {
   return (
-    <header className="flex items-center justify-center h-12 relative bg-white">
+    <header className="flex items-center justify-between h-12 px-4 bg-white">
       {backTo ? (
-        <Link href={backTo} className="absolute left-4 p-2">
+        <Link href={backTo} className="w-6 h-6 flex items-center justify-center">
           <Image src="/icons/back.svg" alt="뒤로가기" width={24} height={24} />
         </Link>
       ) : (
-        <div className="absolute left-4 p-2 w-6 h-6" />
+        <div className="w-6 h-6" />
       )}
-
-      <span className="text-title font-semibold">{title}</span>
-
+      <span className="text-title">{title}</span>
       {rightElement ? (
-        <div className="absolute right-4 p-2">{rightElement}</div>
+        <div className="w-6 h-6 flex items-center justify-center">{rightElement}</div>
       ) : hideNotification ? (
-        <div className="absolute right-4 p-2 w-6 h-6" />
+        <div className="w-6 h-6" />
       ) : (
-        <Link href="/home/notification/setting" className="absolute right-4 p-2">
+        <Link href="/home/notification/setting" className="w-6 h-6 flex items-center justify-center">
           <Image src="/icons/setting.svg" alt="설정" width={24} height={24} />
         </Link>
       )}
