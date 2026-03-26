72ba0839980b0d7f7d214980ac7ed46fdc38d34b
Lee Hyeon Seung <arale3488@gmail.com>
2026-03-27 02:28:50 +0900

    style: 지원자 상세페이지 QuestionSection 디자인 변경

--- src/components/document/QuestionSection.tsx
+++ src/components/document/QuestionSection.tsx
@@ -8,14 +8,14 @@ interface QuestionSectionProps {
 
 export default function QuestionSection({ title, content, maxLength }: QuestionSectionProps) {
   return (
-    <div className="mb-4">
-      <h3 className="text-body-md text-black mb-1">
+    <div className="py-1">
+      <h3 className="text-gray-950 text-[14px] font-medium leading-[20px] [font-variant-numeric:lining-nums_proportional-nums] [font-feature-settings:'dlig'_on] mb-[5px] w-[290px] mx-auto">
         {title}
         <br />
         {maxLength && <span className="text-body-sm">({maxLength}자)</span>}
       </h3>
-      <div className="p-4 rounded-10 bg-gray-50">
-        <p className="py-1 text-body-sm-rg text-gray-800 whitespace-pre-wrap">{content}</p>
+      <div className="w-[310px] rounded-10 bg-[#F3F4F8] pt-[15px] pr-[16px] pb-[15px] pl-[16px]">
+        <p className="text-gray-600 text-[13px] font-normal leading-[22px] tracking-[1%] [font-variant-numeric:lining-nums_proportional-nums] whitespace-pre-wrap">{content}</p>
       </div>
       <Navbar />
     </div>
